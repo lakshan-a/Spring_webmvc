85070923f702b27d1a4cb8a0a4824692accf7656
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 11:32:44 +0530

    updated

--- 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/HelloSpringWebMvc/WebAppConfig.java
+++ 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/HelloSpringWebMvc/WebAppConfig.java
@@ -9,7 +9,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "lk.ijse.gdse66.HelloSpringWebMvc.bean")
 @EnableWebMvc
 public class WebAppConfig {
-    public WebAppConfig() {
-        System.out.println("hello");
-    }
+
 }
