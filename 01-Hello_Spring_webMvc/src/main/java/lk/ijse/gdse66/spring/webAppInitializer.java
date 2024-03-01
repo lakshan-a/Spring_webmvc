a457e4053174a7dd37009ab1cbaf1f67c906dd3d
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 10:38:41 +0530

    updated

--- 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppInitializer.java
+++ 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse66.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  * @version: v0.0.1
@@ -8,11 +9,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @date: 3/1/2024
  */
 
-public class webAppInitializer {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(webAppConfig.class);
-        ctx.refresh();
-        ctx.registerShutdownHook();
+public class webAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
     }
 }
