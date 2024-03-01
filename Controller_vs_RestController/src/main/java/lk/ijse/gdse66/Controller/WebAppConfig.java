990beb3a24fc4842132df0283917887262880276
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 16:29:46 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -25,8 +26,8 @@ public class WebAppConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        WebMvcConfigurer.super.configureViewResolvers(registry);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 
     @Bean
