3665f2f0e61b0b388010e81de223854714e40921
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 15:51:00 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/api/MyController2.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/api/MyController2.java
@@ -1,9 +1,16 @@
 package lk.ijse.gdse66.Controller.api;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
+
+@Controller
 public class MyController2 {
+    public MyController2() {
+        System.out.println("MyController2() - constructor");
+    }
 }
