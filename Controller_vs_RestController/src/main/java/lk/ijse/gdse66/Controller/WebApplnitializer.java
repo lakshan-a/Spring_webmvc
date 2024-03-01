19b5a73435d202300538caef2790b43bd48daf3b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 15:32:43 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebApplnitializer.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebApplnitializer.java
@@ -1,9 +1,25 @@
 package lk.ijse.gdse66.Controller;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
-public class WebApplnitializer {
+public class WebApplnitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
---
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/bean/SpringBean.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse66.Controller.bean;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 3/1/2024
+ */
+public class SpringBean {
+}
