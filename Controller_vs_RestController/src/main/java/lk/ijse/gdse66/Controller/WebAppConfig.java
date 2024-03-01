b1813f702dc9774d5182f8af09922cad6affdd7c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 16:26:54 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -17,7 +18,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("WebAppConfig() - constructor");
     }
