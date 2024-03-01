8def0303c5d27382bce7a511c97e51babe7427e9
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 15:30:16 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/WebAppConfig.java
@@ -1,9 +1,17 @@
 package lk.ijse.gdse66.Controller;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
+
+@Configuration
+//@ComponentScan(basePackages = "lk.ijse.gdse66.Conrtoller")
+@EnableWebMvc
 public class WebAppConfig {
 }
