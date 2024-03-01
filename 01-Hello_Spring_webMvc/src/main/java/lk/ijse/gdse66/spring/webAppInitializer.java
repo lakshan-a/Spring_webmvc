c09ba5e73a69272ac16c032e74c9e49a5ffc1c46
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 10:34:53 +0530

    updated

--- 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppInitializer.java
+++ 01-Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppInitializer.java
@@ -1,9 +1,18 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
+
 public class webAppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(webAppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+    }
 }
