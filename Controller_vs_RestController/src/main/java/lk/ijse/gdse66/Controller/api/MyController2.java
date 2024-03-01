6ccb8d1ed7a8491055aa5ee6cafa87f1c2dbf1ec
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 15:55:30 +0530

    updated

--- Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/api/MyController2.java
+++ Controller_vs_RestController/src/main/java/lk/ijse/gdse66/Controller/api/MyController2.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse66.Controller.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @version: v0.0.1
@@ -9,8 +11,14 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller
+@RequestMapping("/two")
 public class MyController2 {
     public MyController2() {
         System.out.println("MyController2() - constructor");
+    }
+
+    @GetMapping
+    public String getMethod(){
+        return "MyController2";
     }
 }
