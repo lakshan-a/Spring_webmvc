2f62ff4b08c0263954737fa27230bc366477c6ac
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-01 10:26:23 +0530

    updated

--- Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppConfig.java
+++ Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webAppConfig.java
@@ -1,9 +1,18 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
 public class webAppConfig {
 }
--- Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webRootConfig.java
+++ Hello_Spring_webMvc/src/main/java/lk/ijse/gdse66/spring/webRootConfig.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/1/2024
  */
+
+@Configuration
 public class webRootConfig {
 }
