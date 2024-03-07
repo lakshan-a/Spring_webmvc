38cd16319a68590cb271f8d59eb4bc4a5f315c06
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 14:49:19 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
@@ -17,4 +17,37 @@ public class CustomerDTO {
         this.name = name;
         this.age = age;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
