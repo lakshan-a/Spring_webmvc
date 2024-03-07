12d1243e4c4dabb9db5a9aab0f4b46fbb1f94d38
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 14:49:02 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
@@ -5,8 +5,16 @@ package lk.ijse.gdse66.mapping.spec.dto;
  * @author: LakshanR
  * @date: 3/7/2024
  */
+
 public class CustomerDTO {
     private String id;
     private String name;
     private int age;
+
+
+    public CustomerDTO(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
