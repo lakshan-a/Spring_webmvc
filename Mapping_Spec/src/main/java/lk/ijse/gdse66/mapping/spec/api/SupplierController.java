2573f3e270705d6bc4737b487aeb91724a511da5
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 12:51:18 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/SupplierController.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/SupplierController.java
@@ -26,7 +26,7 @@ public class SupplierController {
     }
 
     @GetMapping(params = "age")
-    public String getMethodWithParam3( int age){
+    public String getMethodWithParam3(String id,String name, int age){
         return String.format("getMethodWithParam2(int age): %s",age);
     }
 
