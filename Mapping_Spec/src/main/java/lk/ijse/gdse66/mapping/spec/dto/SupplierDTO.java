4c699a0975d6b356544be2b8b0a81bcdd8d73538
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 15:19:16 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/SupplierDTO.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/SupplierDTO.java
@@ -1,12 +1,18 @@
 package lk.ijse.gdse66.mapping.spec.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/7/2024
  */
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SupplierDTO {
     private String id;
     private String name;
