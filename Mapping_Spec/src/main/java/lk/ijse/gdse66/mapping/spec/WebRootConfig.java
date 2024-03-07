573432724d12acd7507b3b2ea7b80a477bd60206
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 10:11:29 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebRootConfig.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebRootConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.gdse66.mapping.spec;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/7/2024
  */
+
+@Configuration
 public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig () -- Contractor");
+    }
 }
