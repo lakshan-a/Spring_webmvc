ce174363f213e9e827fce9f408bb7b649180af87
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 15:54:39 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/ItemController.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/ItemController.java
@@ -1,9 +1,15 @@
 package lk.ijse.gdse66.mapping.spec.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/7/2024
  */
+
+@RestController
+@RequestMapping("/item")
 public class ItemController {
 }
