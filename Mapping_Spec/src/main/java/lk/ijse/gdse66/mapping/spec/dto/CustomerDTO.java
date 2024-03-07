718a08dbfad4ce73db9ab78f0c97fd746b6df72a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 14:55:49 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/dto/CustomerDTO.java
@@ -1,10 +1,15 @@
 package lk.ijse.gdse66.mapping.spec.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/7/2024
  */
+@Data
+@AllArgsConstructor
 
 public class CustomerDTO {
     private String id;
@@ -12,42 +17,4 @@ public class CustomerDTO {
     private int age;
 
 
-    public CustomerDTO(String id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDTO{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
