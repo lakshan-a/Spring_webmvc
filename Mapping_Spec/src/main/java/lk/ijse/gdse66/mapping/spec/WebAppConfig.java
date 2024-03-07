fa5bb4ae2b88ab45d239d40a8a81a88051d0aea3
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-07 10:10:54 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebAppConfig.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebAppConfig.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse66.mapping.spec;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -7,5 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author: LakshanR
  * @date: 3/7/2024
  */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
 public class WebAppConfig implements WebMvcConfigurer {
+
+    public WebAppConfig() {
+        System.out.println("WebAppConfig () -- Contractor");
+    }
 }
--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebAppInitializer.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/WebAppInitializer.java
@@ -10,12 +10,12 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
