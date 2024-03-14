0afff94a8f514ba71b9cc0910b27c2deca34b2d9
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-14 15:56:59 +0530

    updated

--- Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDTO.java
+++ Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDTO.java
@@ -1,9 +1,22 @@
 package lk.ijse.gdse66.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/14/2024
  */
-public class CustomerDTO {
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO implements Serializable {
+    private String id;
+    private String name;
+    private String address;
 }
