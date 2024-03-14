aabb8db41c86a34b049b9adb6dd2c5b8123ee71c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-14 09:09:13 +0530

    updated

--- Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/OrderController.java
+++ Mapping_Spec/src/main/java/lk/ijse/gdse66/mapping/spec/api/OrderController.java
@@ -1,9 +1,21 @@
 package lk.ijse.gdse66.mapping.spec.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/14/2024
  */
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @GetMapping
+    public String getOrder(){
+        return "getOrder()";
+    }
 }
