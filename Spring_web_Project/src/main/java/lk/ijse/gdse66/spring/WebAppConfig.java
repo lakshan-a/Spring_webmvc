21036467498056037040ee5be4f220984ad28646
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-14 15:45:27 +0530

    updated

--- Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/WebAppConfig.java
+++ Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/WebAppConfig.java
@@ -1,9 +1,17 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/14/2024
  */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.gdse66.mapping.spec.api")
 public class WebAppConfig {
 }
