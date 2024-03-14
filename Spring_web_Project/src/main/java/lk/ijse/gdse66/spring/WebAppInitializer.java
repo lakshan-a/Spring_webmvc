9d4cbe600c4b61dbfc39eabc30f1ae181ed02c39
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-14 15:43:38 +0530

    updated

--- Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
+++ Spring_web_Project/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
@@ -1,9 +1,25 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/14/2024
  */
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
