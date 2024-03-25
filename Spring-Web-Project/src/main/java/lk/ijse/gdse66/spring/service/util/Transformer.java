3e178dd6c9c61877f394bbf30cf0cb6a756998b2
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 15:32:15 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse66.spring.service.util;
 
+import org.modelmapper.ModelMapper;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/25/2024
  */
 public class Transformer {
+
+    ModelMapper mapper;
 }
