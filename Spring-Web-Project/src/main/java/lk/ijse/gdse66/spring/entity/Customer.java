8fdff168d6a647886577a6266364746cf88fa50d
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 14:06:37 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/entity/Customer.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/entity/Customer.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.spring.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,14 @@ import lombok.NoArgsConstructor;
  * @date: 3/25/2024
  */
 
+
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
+    @Id
     private String id;
     private String name;
     private String address;
