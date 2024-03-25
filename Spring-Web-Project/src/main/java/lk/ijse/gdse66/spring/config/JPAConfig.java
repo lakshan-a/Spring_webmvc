50769b78020d5ef016decab90c476ae44dbc5031
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 12:48:23 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
@@ -25,6 +25,7 @@ public class JPAConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dmds = new DriverManagerDataSource();
+        return dmds;
     }
 
     @Bean
