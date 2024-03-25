119796844cfce83bff84c52e430ee9a3d66260e5
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 15:33:04 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
@@ -1,13 +1,18 @@
 package lk.ijse.gdse66.spring.service.util;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/25/2024
  */
+
+@Component
 public class Transformer {
 
+    @Autowired
     ModelMapper mapper;
 }
