9f1bb6784bd19008c7dc1627c8b498501d381d3e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 15:37:14 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/WebRootConfig.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/WebRootConfig.java
@@ -2,9 +2,11 @@ package lk.ijse.gdse66.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {"lk.ijse.gdse66.spring.service","lk.ijse.gdse66.spring.repositories"})
+@Import(JPAConfig.class)
 public class WebRootConfig {
     public WebRootConfig() {
         System.out.println("WebRootConfig () -- Contractor");
