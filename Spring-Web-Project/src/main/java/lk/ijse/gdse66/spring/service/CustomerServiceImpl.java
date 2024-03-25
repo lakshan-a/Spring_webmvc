625dbc8ac33d546193a0a44990ad4ca5d5e41f6f
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 15:01:22 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/CustomerServiceImpl.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/CustomerServiceImpl.java
@@ -22,11 +22,7 @@ public class CustomerServiceImpl implements CustomerService{
 
     ArrayList<CustomerDto> customerList = new ArrayList<>();
 
-    {
-        customerList.add(new CustomerDto("C001","Navishka","Matara"));
-        customerList.add(new CustomerDto("C002","lakshan","Galle"));
-        customerList.add(new CustomerDto("C003","Savinda","Thangalla"));
-    }
+
 
 
     @Override
