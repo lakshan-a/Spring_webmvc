881c5b094e1f528abb1c84bd0a6ac11956b78550
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 09:58:52 +0530

    updated

---
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/servicce/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package lk.ijse.gdse66.spring.servicce;
+
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+
+import java.util.List;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 3/25/2024
+ */
+public class CustomerServiceImpl implements CustomerService{
+    @Override
+    public List<CustomerDto> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public CustomerDto getCustomerDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public void saveCustomer(CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+}
