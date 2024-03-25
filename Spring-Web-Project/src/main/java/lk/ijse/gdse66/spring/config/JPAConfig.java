ee9689e60ef31de5781513e87f953a2de5da5fe2
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 14:04:12 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
@@ -3,6 +3,8 @@ package lk.ijse.gdse66.spring.config;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.val;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -10,6 +12,7 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -19,6 +22,9 @@ import javax.sql.DataSource;
  * @date: 3/25/2024
  */
 
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
 
 public class JPAConfig {
 
