3a44d15a477bc69d1abe502d2d70c1654fd57635
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-25 09:58:01 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/servicce/CustomerService.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/servicce/CustomerService.java
@@ -1,9 +1,22 @@
 package lk.ijse.gdse66.spring.servicce;
 
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/25/2024
  */
 public interface CustomerService {
+    List<CustomerDto> getAllCustomer();
+
+    CustomerDto getCustomerDetail (String id);
+
+    void saveCustomer (CustomerDto customerDto);
+
+    void updateCustomer (CustomerDto customerDto);
+
+    void deleteCustomer (String id);
 }
