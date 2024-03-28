a9e4cc1a443093e0733a63a3807b566beed2d67f
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-28 13:35:24 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/exception/DuplicateRecordException.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/exception/DuplicateRecordException.java
@@ -7,4 +7,7 @@ package lk.ijse.gdse66.spring.service.exception;
  */
 public class DuplicateRecordException extends ServiceException{
 
+    public DuplicateRecordException(String message) {
+        super(message);
+    }
 }
