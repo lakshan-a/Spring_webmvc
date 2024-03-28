f848c638dbe33dbf5bb56d571170b6f15ec08f56
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-28 09:45:53 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/UtilMatter.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/UtilMatter.java
@@ -1,9 +1,14 @@
 package lk.ijse.gdse66.spring.service.util;
 
+import java.util.UUID;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/28/2024
  */
 public class UtilMatter {
+    public static String generateId(){
+        return UUID.randomUUID().toString();
+    }
 }
