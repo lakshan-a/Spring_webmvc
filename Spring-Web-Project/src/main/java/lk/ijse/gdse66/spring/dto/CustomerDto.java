11bd5dc7cf6ff504c5cd3d65229059356714c3c8
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-28 11:53:54 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDto.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDto.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.spring.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,10 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDto implements Serializable {
-    @Null(message = "Id Is Auto Generated")
+    @Null(message = "id is auto generated")
     private String id;
     @NotBlank(message = "name can not be null")
+    @Pattern(regexp = "[A-Za-z]+", message = "name not valid")
     private String name;
     @NotBlank(message = "address can not be null")
     private String address;
