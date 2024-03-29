202925d83a99982dacd97edaed99cfe0a5869930
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-29 19:05:23 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
@@ -1,6 +1,13 @@
 package lk.ijse.gdse66.spring.advice;
 
+import lk.ijse.gdse66.spring.service.exception.DuplicateRecordException;
+import lk.ijse.gdse66.spring.service.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 /**
  * @version: v0.0.1
@@ -10,4 +17,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GobleExceptionHandler {
+
+
 }
