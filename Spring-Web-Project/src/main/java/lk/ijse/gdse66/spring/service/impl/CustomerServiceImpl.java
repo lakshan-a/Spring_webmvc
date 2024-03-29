cf9a044610036062bec3ca4c71c9114eea5acf05
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-29 19:00:19 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImpl.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImpl.java
@@ -24,11 +24,10 @@ import java.util.stream.Stream;
 @Service
 @Transactional
 public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.CustomerService {
-
     @Autowired
     CustomerRepo repo;
 
-//    ArrayList<CustomerDto> customerList = new ArrayList<>();
+    ArrayList<CustomerDto> customerList = new ArrayList<>();
 
 
     @Autowired
@@ -36,20 +35,20 @@ public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.Custom
 
     @Override
     public List<CustomerDto> getAllCustomers() {
-
-        /*List<Customer> customerList =repo.findAll();
-        Stream<Customer> customerStream =customerList.stream();
-        Stream<CustomerDto> customerDtoStream = customerStream.map(customer -> transformer.fromCustomerEntity(customer));
-        List<CustomerDto> customerDtoList =customerDtoStream.collect(Collectors.toList());*/
-
-        return repo.findAll().stream()
+//        List<Customer> customerList = repo.findAll();
+//        Stream<Customer> stream = customerList.stream();
+//        Stream<CustomerDto> objectStream = stream.map(customer -> transformer.fromCustomerEntity(customer));
+//        List<CustomerDto> collect = objectStream.collect(Collectors.toList());
+        return repo.findAll().stream() //strem eke coustomerla polimata hadanwa
                 .map(customer -> transformer.fromCustomerEntity(customer)).toList();
+
     }
 
     @Override
     public CustomerDto getCustomerDetails(String id) {
+
         if (!repo.existsById(id)){
-            throw new NotFoundException("Customer Id: "+ id + "dose not exist");
+            throw new NotFoundException("Customer Id :" + id +"Dones not exis");
         }
         return transformer.fromCustomerEntity(repo.findById(id).get());
     }
@@ -58,13 +57,13 @@ public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.Custom
     public CustomerDto saveCustomer(CustomerDto customerDTO) {
         customerDTO.setId(UtilMatter.generateId());
         return transformer.fromCustomerEntity(repo.save(transformer.toCustomerEntity(customerDTO)));
-//        Customer customer = repo.save(transformer.toCustomerEntity(customerDTO));
+
     }
 
     @Override
     public void updateCustomer(CustomerDto customerDTO) {
         if (!repo.existsById(customerDTO.getId())){
-            throw new NotFoundException("Updated Failed; customer id : " + customerDTO.getId() + "dose not exist");
+            throw new NotFoundException("Update Filled Id :" + customerDTO.getId() +"Does not exist");
         }
         repo.save(transformer.toCustomerEntity(customerDTO));
     }
@@ -72,7 +71,7 @@ public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.Custom
     @Override
     public void deleteCustomer(String id) {
         if (!repo.existsById(id)){
-            throw new NotFoundException("Delete Failed; customer id :  "+ id + "dose not exist");
+            throw new NotFoundException("Delete Filled Id :" + id +"Does not exist");
         }
         repo.deleteById(id);
     }
