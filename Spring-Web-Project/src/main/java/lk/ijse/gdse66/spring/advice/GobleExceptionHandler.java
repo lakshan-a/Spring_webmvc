5dd5fb6fdb5ef0ede43be6a6cd115bb40c3e6d47
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-29 19:06:09 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.spring.advice;
 
 import lk.ijse.gdse66.spring.service.exception.DuplicateRecordException;
 import lk.ijse.gdse66.spring.service.exception.NotFoundException;
+import lk.ijse.gdse66.spring.service.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,22 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GobleExceptionHandler {
+
+    @ExceptionHandler(ServiceException.class)//only handler Services Exception
+    public ResponseEntity<Map<String,Object>> handleServicesException(ServiceException exp){
+        Map<String, Object> errorAttribute = null;
+        if (exp instanceof DuplicateRecordException){
+            getCommonErrorAttribute(HttpStatus.CONFLICT);
+        }else if(exp instanceof NotFoundException){
+            errorAttribute =getCommonErrorAttribute(HttpStatus.NOT_FOUND);
+        }else{
+            errorAttribute =getCommonErrorAttribute(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        errorAttribute.put("message",exp.getMessage());
+
+        return new ResponseEntity<>(errorAttribute,HttpStatus.valueOf((Integer) errorAttribute.get("code")));
+    }
 
     public Map<String,Object> getCommonErrorAttribute(HttpStatus status){
         LinkedHashMap<String,Object> errorAttributes = new LinkedHashMap<>();
