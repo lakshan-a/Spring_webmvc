9eff29f6c33db5a4ded40644cfef9251bb69f563
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-29 19:03:48 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/advice/GobleExceptionHandler.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse66.spring.advice;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 3/29/2024
  */
+
+@RestControllerAdvice
 public class GobleExceptionHandler {
 }
