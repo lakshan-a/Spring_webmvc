b5c32130cf1f97a05fe44106cdfeedc201a2489e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:50:28 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/ItemController.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/ItemController.java
@@ -1,9 +1,17 @@
 package lk.ijse.gdse66.spring.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/2/2024
  */
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/item")
 public class ItemController {
 }
