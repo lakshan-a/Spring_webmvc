41b4a80370023cb94ebf635ab4bb2780bb49e8c9
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:25:53 +0530

    updated

---
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/dto/ItemDto.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse66.spring.dto;
+
+import java.io.Serializable;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 4/2/2024
+ */
+public class ItemDto implements Serializable {
+}
