21750fa7b2b83bed04c606b4594f56c955bf2b8f
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:31:32 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/entity/Item.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/entity/Item.java
@@ -1,9 +1,26 @@
 package lk.ijse.gdse66.spring.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/2/2024
  */
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+
+    @Id
+    private String ItemCode;
+    private String ItemName;
+    private double ItemPrice;
+    private int ItemQty;
 }
