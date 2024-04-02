0bf57ca1aba5acb21f02251d56d6a714c84b2a23
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:41:01 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/ItemServiceImpl.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/ItemServiceImpl.java
@@ -1,9 +1,52 @@
 package lk.ijse.gdse66.spring.service.impl;
 
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+import lk.ijse.gdse66.spring.dto.ItemDto;
+import lk.ijse.gdse66.spring.repositories.CustomerRepo;
+import lk.ijse.gdse66.spring.service.util.Transformer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/2/2024
  */
-public class ItemServiceImpl {
+public class ItemServiceImpl implements lk.ijse.gdse66.spring.service.ItemService {
+
+    @Autowired
+    CustomerRepo repo;
+
+    ArrayList<CustomerDto> customerList = new ArrayList<>();
+
+
+    @Autowired
+    Transformer transformer;
+
+    @Override
+    public List<ItemDto> getAllItems() {
+        return null;
+    }
+
+    @Override
+    public ItemDto getItemDetails(String id) {
+        return null;
+    }
+
+    @Override
+    public ItemDto saveItem(ItemDto itemDto) {
+        return null;
+    }
+
+    @Override
+    public void updateItem(ItemDto itemDto) {
+
+    }
+
+    @Override
+    public void deleteItem(String id) {
+
+    }
 }
