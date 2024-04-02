1e1c118623d8f7c5e914318d95080191d785745b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:52:21 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/ItemController.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/ItemController.java
@@ -1,8 +1,17 @@
 package lk.ijse.gdse66.spring.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+import lk.ijse.gdse66.spring.dto.ItemDto;
+import lk.ijse.gdse66.spring.service.CustomerService;
+import lk.ijse.gdse66.spring.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
+import java.util.List;
 
 /**
  * @version: v0.0.1
@@ -14,4 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/item")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ItemDto> getAllItems(){
+        return itemService.getAllItems();
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveItem(@Valid @RequestPart("id") String id,
+                             @RequestPart("name") String name,
+                             @RequestPart("address") String address,
+                             @RequestPart("profilePic") String profilePic) {
+        String base64ProfilePic = Base64.getEncoder().encodeToString(profilePic.getBytes());
+        ItemDto item = new ItemDto(id, name, address, base64ProfilePic);
+        itemService.saveItem(item);
+    }
 }
