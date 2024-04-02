e1da21b32c8077de77f1d78a014047895a5950f7
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-02 11:37:22 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/repositories/ItemRepo.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/repositories/ItemRepo.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse66.spring.repositories;
 
+import lk.ijse.gdse66.spring.entity.Customer;
+import lk.ijse.gdse66.spring.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/2/2024
  */
-public interface ItemRepo {
+public interface ItemRepo extends JpaRepository<Item,String> {
 }
