a21751f6af60c6134f5ce112583e435be486a84e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 15:48:42 +0530

    updated

---
+++ hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse66.hello_springboot.dto;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 4/4/2024
+ */
+public class CustomerDTO {
+}
--- hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/service/CustometrService.java
+++ hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/service/CustometrService.java
@@ -1,10 +1,19 @@
 package lk.ijse.gdse66.hello_springboot.service;
 
+import lk.ijse.gdse66.hello_springboot.dto.CustomerDTO;
+
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/4/2024
  */
 
-public class CustometrService {
+public interface CustometrService {
+    List<CustomerDTO> getAllCustomer();
+    CustomerDTO getCustomerDetails(String id);
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
+    void updatedCustomer(String id, CustomerDTO customerDTO);
+    void deleteCustomer(String id);
 }
