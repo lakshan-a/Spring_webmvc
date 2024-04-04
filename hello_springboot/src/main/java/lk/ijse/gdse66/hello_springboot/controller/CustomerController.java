57b1f35e1cb7574c4f29b4a5c2f2ef2818225fd2
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 16:14:17 +0530

    updated

--- hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/controller/CustomerController.java
+++ hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/controller/CustomerController.java
@@ -1,8 +1,14 @@
 package lk.ijse.gdse66.hello_springboot.controller;
 
 import jdk.jfr.Registered;
+import lk.ijse.gdse66.hello_springboot.dto.CustomerDTO;
+import lk.ijse.gdse66.hello_springboot.service.CustomerService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @version: v0.0.1
@@ -13,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
+
+
+    CustomerService customerService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    List<CustomerDTO> getAllCustomer(){
+        return customerService.getAllCustomer();
+    }
 }
