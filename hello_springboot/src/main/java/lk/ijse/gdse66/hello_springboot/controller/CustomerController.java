5b752b88d797cc1f4572359456915bc7bb03355d
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 16:12:30 +0530

    updated

--- hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/controller/CustomerController.java
+++ hello_springboot/src/main/java/lk/ijse/gdse66/hello_springboot/controller/CustomerController.java
@@ -1,9 +1,16 @@
 package lk.ijse.gdse66.hello_springboot.controller;
 
+import jdk.jfr.Registered;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/4/2024
  */
+
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
 }
