589ce5a9cfa7edc248a00657893469d816842904
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 09:39:38 +0530

    updated

--- Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImplTest.java
+++ Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImplTest.java
@@ -1,13 +1,27 @@
 package lk.ijse.gdse66.spring.service.impl;
 
+import lk.ijse.gdse66.spring.config.WebRootConfig;
+import lk.ijse.gdse66.spring.service.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/4/2024
  */
+
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = WebRootConfig.class)
+//@SpringJUnitConfig
 public class CustomerServiceImplTest {
+
+//    @Autowired
+//    CustomerService customerService;
 
     @Test
     void getAllCustomers() {
@@ -19,6 +33,7 @@ public class CustomerServiceImplTest {
 
     @Test
     void saveCustomer() {
+
     }
 
     @Test
