465fb3d9c3cd3e342b20af7968d9ec49ee580c09
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 10:04:20 +0530

    updated

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/util/Transformer.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse66.spring.service.util;
 
 import lk.ijse.gdse66.spring.dto.CustomerDto;
+import lk.ijse.gdse66.spring.dto.ItemDto;
 import lk.ijse.gdse66.spring.entity.Customer;
+import lk.ijse.gdse66.spring.entity.Item;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,5 +33,15 @@ public class Transformer {
     public Customer toCustomerEntity(CustomerDto customerDTO){
         Customer customer = mapper.map(customerDTO, Customer.class);
         return customer;
+    }
+
+    public ItemDto fromItemEntity(Item item){
+        ItemDto itemDto = mapper.map(item, ItemDto.class);
+        return itemDto;
+    }
+
+    public Item toItemEntity(ItemDto itemDto){
+        Item item = mapper.map(itemDto, Item.class);
+        return item;
     }
 }
