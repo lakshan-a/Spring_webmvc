fc7837a067213f8dcec8fc23bd5867675266fad6
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-04 12:19:24 +0530

    updated

--- Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/service/impl/repositories/CustomerRepoTest.java
+++ Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/service/impl/repositories/CustomerRepoTest.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse66.spring.service.impl.repositories;
 
 import lk.ijse.gdse66.spring.config.WebRootConfig;
+import lk.ijse.gdse66.spring.dto.CustomerDto;
 import lk.ijse.gdse66.spring.entity.Customer;
 import lk.ijse.gdse66.spring.repositories.CustomerRepo;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -24,12 +27,30 @@ class CustomerRepoTest {
     @Autowired
     CustomerRepo customerRepo;
 
+    void addAllCustomers(){
+        Customer customer1 = new Customer("C001", "Kasun Sampath", "Galle", "ProfilePic1");
+        Customer customer2 = new Customer("C002", "Saman Kumara", "Matara", "ProfilePic2");
+        Customer customer3 = new Customer("C003", "Amal Perera", "Panadura", "ProfilePic3");
+
+        customerRepo.save(customer1);
+        customerRepo.save(customer2);
+        customerRepo.save(customer3);
+    }
+
     @Test
     void saveCustomer(){
         Customer customer = new Customer("C001", "Navishka", "Galle", "Photo");
         Customer saveCustomer = customerRepo.save(customer);
 
         assertNotNull(saveCustomer);
+    }
+
+    void getCustomerByName(){
+        addAllCustomers();
+        List<Customer> customerList = customerRepo.findCustomersByName("lakshan");
+        for (Customer customer : customerList){
+            System.out.println(customer);
+        }
     }
 
 }
