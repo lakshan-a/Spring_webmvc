8f942b68f522ca8d118e393d1a6636d1144061a4
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 12:30:55 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/dto/UserDTO.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/dto/UserDTO.java
@@ -1,9 +1,26 @@
 package lk.ijse.gdse66.springbootwithjwt.dto;
 
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
