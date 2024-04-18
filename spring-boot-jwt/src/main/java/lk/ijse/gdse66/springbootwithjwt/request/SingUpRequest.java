4fa501bd1d893678e10c5c1b8d81667e49bc846b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 16:40:20 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/request/SingUpRequest.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/request/SingUpRequest.java
@@ -1,9 +1,26 @@
 package lk.ijse.gdse66.springbootwithjwt.request;
 
+import lk.ijse.gdse66.springbootwithjwt.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SingUpRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
 }
