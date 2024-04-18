f331fecb4a88e719608f67c6e56edd7c8ea6e52d
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 14:27:48 +0530

    updated

---
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/config/JwdConfigurationFilter.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse66.springbootwithjwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 4/18/2024
+ */
+
+@Configuration
+public class JwdConfigurationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
