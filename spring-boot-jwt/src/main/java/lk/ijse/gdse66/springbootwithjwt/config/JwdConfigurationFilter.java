21b1a55f196dc960aa1d35d894182ce3bb640750
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 14:34:28 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/config/JwdConfigurationFilter.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/config/JwdConfigurationFilter.java
@@ -20,6 +20,8 @@ public class JwdConfigurationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authenticationHeader = request.getHeader("AuthenticationHeader");
-        if (authenticationHeader != null && authenticationHeader.startsWith("Bearer"));
+        if (authenticationHeader != null && authenticationHeader.startsWith("Bearer")){
+            String jwt = authenticationHeader.substring(7);
+        }
     }
 }
