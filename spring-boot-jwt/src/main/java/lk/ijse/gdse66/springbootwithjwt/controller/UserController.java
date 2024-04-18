8ff873e862651305a189c5651d09d40f2d1e05bf
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 12:16:23 +0530

    updated

---
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/controller/UserController.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse66.springbootwithjwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 4/18/2024
+ */
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class UserController {
+}
