30651376c2acf33a3323f8a3c2af3461b0eef5db
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 16:40:40 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/request/SingInRequest.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/request/SingInRequest.java
@@ -1,9 +1,21 @@
 package lk.ijse.gdse66.springbootwithjwt.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SingInRequest {
+    private String email;
+    private String password;
 }
