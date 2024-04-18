285eb1c90525fa2f5f0a97e9b72182464cc23dad
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 14:38:40 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/JwdService.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/JwdService.java
@@ -1,9 +1,16 @@
 package lk.ijse.gdse66.springbootwithjwt.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
 public interface JwdService {
+    String extractUserName(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
