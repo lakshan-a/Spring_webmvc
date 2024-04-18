9049e1b5040d6cc6125de9b57b6d9651979f60b0
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 12:36:09 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/impl/UserServiceImpl.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/impl/UserServiceImpl.java
@@ -1,9 +1,26 @@
 package lk.ijse.gdse66.springbootwithjwt.service.impl;
 
+import lk.ijse.gdse66.springbootwithjwt.dto.UserDTO;
+import lk.ijse.gdse66.springbootwithjwt.service.UserService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
-public class UserServiceImpl {
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDetailsService userDetailService() {
+        return null;
+    }
+
+    @Override
+    public void Save(UserDTO userDTO) {
+
+    }
 }
