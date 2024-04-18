87ce7e3c266c29cc14184f1a66cb1c7ce794a776
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 16:44:02 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/auth/response/JwtAuthResponse.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/auth/response/JwtAuthResponse.java
@@ -1,9 +1,20 @@
 package lk.ijse.gdse66.springbootwithjwt.auth.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class JwtAuthResponse {
+    private String token;
 }
