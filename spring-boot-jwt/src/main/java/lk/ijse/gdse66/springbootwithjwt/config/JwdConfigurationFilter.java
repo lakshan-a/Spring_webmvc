48fd2979fc26b938bff22bc8ed651780457c582e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 15:56:52 +0530

    updated

--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/config/JwdConfigurationFilter.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/config/JwdConfigurationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -27,7 +28,8 @@ public class JwdConfigurationFilter extends OncePerRequestFilter {
         String authenticationHeader = request.getHeader("AuthenticationHeader");
         if (authenticationHeader != null && authenticationHeader.startsWith("Bearer")){
             String jwt = authenticationHeader.substring(7);
-            if (ext)
+            String extractedUserName = jwtService.extractedUserName(jwt);
+            if (extractedUserName != null && SecurityContextHolder.getContext().getAuthentication()==null);
         }
     }
 }
