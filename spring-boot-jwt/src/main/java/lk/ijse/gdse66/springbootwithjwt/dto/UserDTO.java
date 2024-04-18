f9fe2b1b26265e4142615e4a672fb3b9321f499a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-04-18 12:29:41 +0530

    updated

---
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse66.springbootwithjwt.dto;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 4/18/2024
+ */
+public class UserDTO {
+}
--- spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/UserService.java
+++ spring-boot-jwt/src/main/java/lk/ijse/gdse66/springbootwithjwt/service/UserService.java
@@ -1,9 +1,14 @@
 package lk.ijse.gdse66.springbootwithjwt.service;
 
+import lk.ijse.gdse66.springbootwithjwt.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 /**
  * @version: v0.0.1
  * @author: LakshanR
  * @date: 4/18/2024
  */
 public interface UserService {
+    UserDetailsService userDetailService();
+    void Save (UserDTO userDTO);
 }
